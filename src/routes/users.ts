a241749552c511b9ed6c8732018782a2c493a9f5
Moises Sanchez <djmoisesas@gmail.com>
2026-06-11 10:10:15 -0400

    add clean-test endpoint to delete test users

--- src/routes/users.ts
+++ src/routes/users.ts
@@ -15,6 +15,20 @@ router.get('/list', async (_req: Request, res: Response) => {
   res.json(users);
 });
 
+router.post('/clean-test', async (_req: Request, res: Response) => {
+  const db = getDb();
+  const ids = [
+    '00fec39a-1e89-436b-9df8-8a9455c9665b',
+    'df54f0e6-a5a6-45c7-b277-e6dc4f8a632e',
+    '37d2a707-f9ad-4221-9855-1b1c1c53ca65',
+    '1b959fd0-9031-45d7-b2eb-c4f6ec51a97f',
+    '63f4d5da-8d0b-4bad-9aa9-7d360ae00773',
+    'c7945611-d6a2-4a52-9664-f57f34de7ed9',
+  ];
+  await db.query('DELETE FROM users WHERE id = ANY($1::text[])').run(ids);
+  res.json({ deleted: ids.length });
+});
+
 router.get('/', async (_req: Request, res: Response) => {
   const db = getDb();
   const users = await db.query(
